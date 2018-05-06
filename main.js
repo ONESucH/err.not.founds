b7159f9013bab3f753cc3289a9bb2b82aef6b517
Onesuch <onesuch07@gmail.com>
2018-05-06 17:08:47 +0300

    2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 $(document).ready(function() {
     $.ajax({
-        url: 'http://api.ipstack.com/check?access_key=99d528343eb31080f6bfba4e5255ee24',
+        url: '//api.ipstack.com/check?access_key=99d528343eb31080f6bfba4e5255ee24',
         success: function(data) {
             console.log('success', data);
 
